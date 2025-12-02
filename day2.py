7da36f2da415acd28feee7c55fd7329dc25cb0e7
Ingmar Blonk <ingmar.blonk@gmail.com>
2025-12-02 20:31:52 +0100

    Day 2 cleanup

--- day2.py
+++ day2.py
@@ -10,15 +10,16 @@ for id_range in aoc.input().split(','):
     while a <= b:
         s = str(a)
 
+        # Find ID's of which both halves equal
         h1 = s[:len(s) // 2]
         h2 = s[len(s) // 2:]
         if h1 == h2:
             part1 += a
 
-        token_ranges = ((i, j) for i in range(len(s) // 2) for j in range(1, len(s) // 2 + 1))
-        for i, j in token_ranges:
-            if i < j and (len(s) % (j-i)) == 0:
-                token = s[i:j]
+        # Find ID's of which some sequence of digits repeat at least twice
+        for end in range(1, len(s) // 2 + 1):
+            if len(s) % end == 0:
+                token = s[:end]
                 multiple = len(s) // len(token)
                 if token * multiple == s:
                     part2 += a
