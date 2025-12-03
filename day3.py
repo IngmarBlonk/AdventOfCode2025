6c9dbce8051e84886a027d69e8389235beef026e
Ingmar Blonk <ingmar.blonk@gmail.com>
2025-12-03 19:58:40 +0100

    Day 3 cleanup

--- day3.py
+++ day3.py
@@ -1,20 +1,20 @@
 import aoc
 
+def calc_joltage(bank, battery_count):
+    joltage = 0
+    for i in reversed(range(battery_count)):
+        battery = max(bank[:-i] if i else bank)
+        bank = bank[bank.index(battery) + 1:]
+        joltage = joltage * 10 + int(battery)
+    return joltage
+
+
 part1 = 0
 part2 = 0
 
 for bank in aoc.lines(example=False):
-    d1 = max(bank[:-1])
-    d2 = max(bank[bank.index(d1) + 1:])
-    joltage = int(d1 + d2)
-    part1 += joltage
-
-    joltage = 0
-    for i in reversed(range(12)):
-        battery = max(bank[:-i] if i else bank)
-        bank = bank[bank.index(battery) + 1:]
-        joltage = joltage * 10 + int(battery)
-    part2 += joltage
+    part1 += calc_joltage(bank, 2)
+    part2 += calc_joltage(bank, 12)
 
 print(part1)
 print(part2)
