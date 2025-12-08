43c2dbd9cb7999c2969a6ccf341e4bd4a15624b5
Ingmar Blonk <ingmar.blonk@gmail.com>
2025-12-08 20:23:33 +0100

    Day 8 part 2

--- day8.py
+++ day8.py
@@ -3,40 +3,46 @@ from collections import namedtuple
 from itertools import combinations
 import aoc
 
-example = False
+example = False  # Example input data switch
 PAIRS = 10 if example else 1000
 
 Point = namedtuple("Point", "x y z id")
 
+# Parse box coordinates
 boxes = []
 for i, line in enumerate(aoc.lines(example=example)):
     x, y, z = map(int, line.split(','))
     boxes.append(Point(x, y, z, i))
 
 def dist(p1, p2):
+    """Calculate the straight-line distance between 2 point"""
     return math.sqrt(
         math.pow(p1.x - p2.x, 2) +
         math.pow(p1.y - p2.y, 2) +
         math.pow(p1.z - p2.z, 2)
     )
 
+# Box circuits
 circuit_boxes = {b: [b] for b in boxes}
 circuits = list(circuit_boxes.values())
-for b1, b2 in sorted(combinations(boxes, 2), key=lambda c: dist(*c))[:PAIRS]:
-    # print("connecting", b1.id, b2.id)
-    if circuit_boxes[b1] is not circuit_boxes[b2]:
-        merged_circuit = circuit_boxes[b2]
-        circuit_boxes[b1].extend(merged_circuit)
-        # print("REMOVING", b2.id, merged_circuit)
-        for b in merged_circuit:
-            # print("UPDATE", b.id)
+
+# Connect circuits in order of shortest distance
+for i, (b1, b2) in enumerate(sorted(combinations(boxes, 2), key=lambda c: dist(*c))):
+    if circuit_boxes[b1] is not circuit_boxes[b2]:  # Skip already connected circuits
+        merge_circuit = circuit_boxes[b2]
+
+        for b in merge_circuit:
             circuit_boxes[b] = circuit_boxes[b1]
-        circuits.remove(merged_circuit)
-    # else:
-    #     print("SKIP")
 
-# for circuit in sorted(circuits, key=len, reverse=True)[:10]:
-#     print(len(circuit), circuit)
+        circuit_boxes[b1].extend(merge_circuit)
+        circuits.remove(merge_circuit)
 
-p1 = math.prod(sorted(map(len, circuits), reverse=True)[:3])
-print(p1)
+    if i+1 == PAIRS:
+        # Part 1 - Product of top 3 circuit sizes after {PAIRS} connections made
+        p1 = math.prod(sorted(map(len, circuits), reverse=True)[:3])
+        print(p1)
+    elif len(circuits) == 1:
+        # Part 2 - Wall distance after connecting all into one big circuit
+        p2 = b1.x * b2.x
+        print(p2)
+        break
